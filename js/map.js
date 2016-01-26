088fef76c6942761ea0dd493f0a5e1520d7f7ebe
jbaclig <jon@baclig.com>
2016-01-25 21:54:20 -0800

    add autocomplete search bar to map

--- js/map.js
+++ js/map.js
@@ -5,6 +5,17 @@ function initMap() {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
+
+  //Creat search box and link to UI element
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  //Bias the SearchBox results towards current map's viewport
+  map.addListener('bounds_changed',function(){
+    searchBox.setBounds(map.getBounds());
+  });
+
   var infoWindow = new google.maps.InfoWindow({map: map});
 
   if(navigator.geolocation) {
@@ -24,6 +35,60 @@ function initMap() {
   } else {
     handleLocationError(false,infoWindow,map.getCenter());
   }
+
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed',function() {
+    var places = searchBox.getPlaces();
+
+    if(places.length==0) {
+      return;
+    }
+
+    //Clear out old markers
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    //For each place, get the icon, name, and location
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71,71),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(17,34),
+        scaledSize: new google.maps.Size(25,25)
+      };
+
+      //Create a marker for each place
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if(place.geometry.viewport) {
+        //Only geocodes have viewport
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location)
+      }
+
+      //Update forecast info
+      var latLng = bounds.getCenter();
+      var lat = latLng.lat();
+      var lng = latLng.lng();
+      $('#forecast').remove();
+      getForecast(lat,lng);
+    });
+    map.fitBounds(bounds);
+  });
+  //[END region_getplaces]
 }
 
 function handleLocationError(browserHasGeolocation,infoWindow,pos) {
